ba3859dc9d8449a90574a5c2071db4852fde7a96
DairoMalagonTyK <dairomalagontique@gmail.com>
2025-05-21 04:04:50 -0500

    Arreglando el pq no sale nada

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // ⛓️ Importar rutas o lógica aquí
 require("./backend-fuseki"); // ✅ Solo lo importas, sin usar app.use
 // 🟢 Ruta de prueba
-app.get("/", (req, res) => res.send("API de Reservas Deportivas"));
+app.get("/", (req, res) => {
+  res.send("API de Reservas Deportivas funcionando correctamente ✅");
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
